8c497c0d4454953d0aef3a20a4ad5ae6ca89030c
ETTA EGBE Joseph <ettaegbe40@gmail.com>
2020-02-11 12:46:12 +0100

    initial commit

---
+++ src/Facades/Agent.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Icemix\Agent\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Agent extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'agent';
+    }
+}
